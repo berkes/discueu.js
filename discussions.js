c2353b305d7166b9c2a53eaea6b2e16b7f8c7a17
Bèr Kessels <ber@berk.es>
2022-10-12 16:47:52 +0200

    Add initial discussions library.

---
+++ discussions.js
@@ -0,0 +1,101 @@
+window.addEventListener('load', function(_event) {
+  class Discussions {
+    constructor(repo, renderer) {
+      this.repo = repo;
+      this.renderer = renderer;
+
+      this.discussions = [];
+      this.networks = {
+        'r': 'Reddit',
+        'h': 'Hacker News',
+        'l': 'Lobste.rs',
+      };
+    }
+
+    async maybeHandle() {
+      if (!this.renderer.canRender()) {
+        return;
+      }
+
+      this.renderer.showLoading();
+      this.discussions = await this.repo.get(this.networks);
+      if (this.discussions) {
+        this.renderer.showDiscussions(this.discussions);
+      } else {
+        this.renderer.showError();
+      }
+    }
+  }
+
+  class discussionsRepo {
+    constructor(discussionsUrl) {
+      this.apiKey = 'test';
+      this.fetchUrl = `https://discu.eu/api/v0/discussions/url/${encodeURIComponent(discussionsUrl)}`;
+    }
+
+    async get(networks) {
+      const allowed_ids = Object.keys(networks);
+      const response = await fetch(this.fetchUrl, {
+        credentials: 'omit',
+        mode: 'cors',
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': `Bearer ${this.apiKey}`
+        }
+      });
+
+      if (response.ok) {
+        const json = await response.json();
+        return json.map((discussion) => allowed_ids.includes(discussion.platform) ? { ...discussion, title: networks[discussion.platform] } : null)
+          .filter((discussion) => discussion !== null);
+      } else {
+        return null;
+      }
+    }
+  }
+
+  class templateRenderer {
+    constructor() {
+      this.commentTemplate = document.getElementById('comment-line').content;
+      this.loadingTemplate = document.getElementById('comment-loading').content;
+      this.emptyTemplate = document.getElementById('comment-empty').content;
+      this.errorTemplate = document.getElementById('comment-error').content;
+      this.commentNode = document.getElementById('comment');
+    }
+
+    showLoading() {
+      this.commentNode.appendChild(this.loadingTemplate.cloneNode(true));
+    }
+
+    showError() {
+      this.commentNode.querySelector('.loading').remove();
+      this.commentNode.appendChild(this.errorTemplate.cloneNode(true));
+    }
+
+    showDiscussions(discussions) {
+      this.commentNode.querySelector('.loading').remove();
+
+      if (discussions.length === 0) {
+        this.commentNode.appendChild(this.emptyTemplate.cloneNode(true));
+      } else {
+        discussions.map((discussion) => {
+          let commentTemplate = this.commentTemplate.cloneNode(true);
+          commentTemplate.querySelector('a').href = discussion.discussion_url;
+          let title = discussion.subreddit ? `${discussion.title} r/${discussion.subreddit}` : discussion.title;
+          commentTemplate.querySelector('.platform').innerText = title;
+          commentTemplate.querySelector('.count').innerText = discussion.comment_count;
+          this.commentNode.querySelector('ul').appendChild(commentTemplate);
+        });
+      }
+    }
+
+    canRender() {
+      return this.commentNode !== null;
+    }
+  };
+
+  let discussionsUrl = new URL(document.getElementById("comment").dataset.discussionUrl);
+  let repo = new discussionsRepo(discussionsUrl);
+  let renderer = new templateRenderer();
+  new Discussions(repo, renderer).maybeHandle();
+}); 
